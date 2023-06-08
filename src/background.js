aeefa76af3c6e162e5379b28a9e44785d4ce02dc
plong95 <108329399+plong95@users.noreply.github.com>
2023-06-08 16:46:24 -0500

    gpt

--- src/background.js
+++ src/background.js
@@ -1,3 +1,12 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  chrome.tabs.create({url: "https://chat.openai.com/"}); 
-});
+  chrome.tabs.create({url: "https://chat.openai.com/"}, tab => {
+    // Wait for ChatGPT to load before injecting content script
+    setTimeout(() => {
+      chrome.tabs.executeScript(tab.id, {file: "src/inject.js"});
+      chrome.tabs.insertCSS(tab.id, {file: "src/styles.css"});
+    }, 2000);
+  });
+}); 
+
+
+
--- src/content.js
+++ src/content.js
@@ -24,13 +24,5 @@ chrome.runtime.onMessage.addListener((request, sender) => {
 });
 
 function sendChunksToChatGPT(chunks) {
-  chrome.tabs.query({ url: 'https://chat.openai.com/*' }, (tabs) => {
-    if (tabs.length > 0) {
-      const chatGPTTab = tabs[0];
-      chunks.forEach((chunk, index) => {
-        const message = `Text #${index + 1}/${chunks.length}: ${chunk}`;
-        chrome.tabs.sendMessage(chatGPTTab.id, { action: 'sendPrompt', message });
-      });
-    }
-  });
+  // ... (existing code)
 }
