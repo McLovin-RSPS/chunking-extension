f62e72ef1e719a324a7b46746477c06643e93722
plong95 <108329399+plong95@users.noreply.github.com>
2023-06-08 16:07:01 -0500

    # On branch main # Your branch is up to date with 'origin/main'. # # Changes to be committed: #	modified:   src/inject.js # # Changes not staged for commit: #	modified:   manifest.json #	modified:   src/background.js #	modified:   src/content.js #	deleted:    src/popup.js # # Untracked files: #	popup.js

--- src/inject.js
+++ src/inject.js
@@ -1,3 +1,5 @@
 const textarea = document.querySelector(".tox-textareay");
+// Get text content from active tab 
+const text = textarea.value;
 // Paste chunks and send keypress 
-chrome.runtime.sendMessage({chunks: chunks}, response => {});
+chrome.runtime.sendMessage({chunks: chunks, text, chunkSize}, response => {});
